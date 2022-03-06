2e29a06d71025c288f67b6c55f96073b992aa594
Dknowles <dknowles4489@gmail.com>
2022-03-06 15:27:26 -0500

    added array of questions

---
+++ assets/js/script.js
@@ -0,0 +1,28 @@
+// Questions Array
+const questions = [
+    {
+        question: "What does the acronym DOM stand for ?",
+        answer: "Document Object Model",
+        options: ["Documents Obviously Mine", "Don't Over Mine", "Documents Optional Man", "Document Object Model"],
+    },
+    {
+        question: "What does API stand for ?",
+        answer: "Application Programming Interface",
+        options: ["Application Programming Interface", "Appliances Prepare Ingrediants", "Application Properly Intact", "Apple Pin Integar"],
+    },
+    {
+        question: "What is a callback function ?",
+        answer: "Passing a function into another function",
+        options: ["Adding the element into another element", "Passing a function into another function", "Giving an element a phone number and calling it back later", "returning the text content of an element"],
+    },
+    {
+        question: "What is a DOM method ?",
+        answer: "DOM methods are the actions that you can perform on the objects of DOM",
+        options: ["DOM methods are when you pass a function into another function", "DOM methods are the actions that you can perform on the objects of DOM", "DOM methods are contagious", "DOM methods are used to traverse your bathroom" ],
+    },
+    {
+        question: "Which of these methods will return a interger ?",
+        answer: "parseInt()",
+        options: ["return()", "getAttribute", "parseInt()", "test()"],
+    },
+];
